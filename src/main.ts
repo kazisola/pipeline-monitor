a66f406d9445a47f6bdea335c00af1b09395f387
Kazi Solayman <kazisolayman123@gmail.com>
2026-06-04 17:37:07 -0500

    Updated main with setting up everything together from global validation tor cors policy, and routings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,52 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
+
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Global Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+
+      forbidNonWhitelisted: true,
+
+      transform: true,
+    }),
+  );
+
+  // CORS BEHAVE DUDEEE
+  app.enableCors({
+    origin: 'http://localhost:5173',
+
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+
+    credentials: true,
+  });
+
+  // Global Route Prefix
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(
+    `🚀 Pipeline Monitor API running on http://localhost:${port}/api`,
+  );
+  console.log(`📊 MongoDB Express UI: http://localhost:8081`);
+  console.log(`🔐 Keycloak Admin: http://localhost:8080`);
 }
+
 bootstrap();
